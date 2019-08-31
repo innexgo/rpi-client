3cd06006e81bca6ffb224cedc6d3e92d8e1109ac
ok <ok>
2019-08-31 20:00:43 +0100

    yeet

--- client.py
+++ client.py
@@ -1,41 +1,15 @@
 #!/usr/bin/env python
 
-import RPi.GPIO as GPIO
-import MFRC522
-import signal
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
 
-continue_reading = True
+reader = SimpleMFRC522()
 
-# Capture SIGINT for cleanup when the script is aborted
-def end_read(signal,frame):
-    global continue_reading
-    print("Ctrl+C captured, ending read.")
-    continue_reading = False
+try:
+    while True:
+        id, text = reader.read()
+        print(id)
+except KeyboardInterrupt:
     GPIO.cleanup()
-
-# Hook the SIGINT
-signal.signal(signal.SIGINT, end_read)
-
-# Create an object of the class MFRC522
-MIFAREReader = MFRC522.MFRC522()
-
-# Welcome message
-print("Welcome to the MFRC522 data read example")
-print("Press Ctrl-C to stop.")
-
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-while continue_reading:
-    # Scan for cards
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-
-    # If a card is found
-    if status == MIFAREReader.MI_OK:
-        print("Card detected")
-    # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
-
-    # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
-
-        # Print UID
-        print("Card read UID: %s,%s,%s,%s") % (uid[0], uid[1], uid[2], uid[3])
+    raise
