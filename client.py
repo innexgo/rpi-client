d0fc93662db2fd4856005137bce3b45ba9f80e10
ok <ok>
2019-09-10 23:14:03 +0100

    big changes

--- client.py
+++ client.py
@@ -6,16 +6,69 @@ import time
 import json
 import mfrc522
 import requests
-import RPi.GPIO as GPIO
+import operator
+import threading
+import RPi.GPIO
+
+courses = None
+periods = None
+
+apiKey = None
+hostname = None
+locationId = None
+
+currentPeriod = None
+currentCourse = None
+
+def currentMillis():
+    return round(1000 * time.time())
+
+# setInterval for python
+def setInterval(func, sec):
+    def func_wrapper():
+        setInterval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+
+updateInfo():
+    global currentCourse
+    global currentPeriod
+
+    millis = currentMillis()
+    currentPeriod = periods.filter(lambda p : p['initialTime'] > millis)
+                           .sort(
+
+
+def updateInfoInfrequent():
+    global periods
+    global courses
+    courses = json.loads(requests.get(hostname + 'course/' +
+                                             '?locationId='+locationId +
+                                             '&count=20' +
+                                             '&apiKey='+apiKey).json())
+
+    periods = json.loads(requests.get(hostname + 'period/' +
+                                             '?initialTimeBegin=' +
+                                             '&count=20' +
+                                             '&apiKey='+apiKey).json())
+    updateInfo()
 
 # Load the config file
-with open('/boot/innexgo-client.json') as configfile:
+with open('innexgo-client.json') as configfile:
     config = json.load(configfile)
 
-    #apiKey = config['apiKey']
-    #hostname = config['hostname']
-    #locationId = config['locationId']
+    apiKey = config['apiKey']
+    hostname = config['hostname']
+    locationId = config['locationId']
 
+    if apiKey is None or hostname is None or locationId is None:
+        print('error reading the json')
+        sys.exit()
+
+    courses = 
 
     # now we can get scanning
     reader = mfrc522.MFRC522()
