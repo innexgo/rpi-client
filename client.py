950e9b98cc35f2b4562f423a5f5980a9c4f269dc
gpi <gpimpale29@gmail.com>
2019-09-11 10:47:58 -0700

    more work unstable

--- client.py
+++ client.py
@@ -4,16 +4,17 @@ import os
 import sys
 import time
 import json
-import mfrc522
+# import mfrc522
 import requests
 import operator
 import threading
-import RPi.GPIO
+#import RPi.GPIO
 
 courses = None
 periods = None
 
 apiKey = None
+protocol = None
 hostname = None
 locationId = None
 
@@ -33,54 +34,61 @@ def setInterval(func, sec):
     return t
 
 
-updateInfo():
+def updateInfo():
     global currentCourse
     global currentPeriod
 
     millis = currentMillis()
-    currentPeriod = periods.filter(lambda p : p['initialTime'] > millis)
-                           .sort(
+    print(periods)
+    currentPeriod = filter(lambda p : (p['initialTime'] > millis), periods)[0]
+    print(currentPeriod)
+
+    currentCourse = filter(lambda c : (c['period'] == currentPeriod['period']), courses)[0]
+    print(currentCourse)
 
 
 def updateInfoInfrequent():
     global periods
     global courses
-    courses = json.loads(requests.get(hostname + 'course/' +
-                                             '?locationId='+locationId +
-                                             '&count=20' +
-                                             '&apiKey='+apiKey).json())
 
-    periods = json.loads(requests.get(hostname + 'period/' +
-                                             '?initialTimeBegin=' +
-                                             '&count=20' +
-                                             '&apiKey='+apiKey).json())
+    millis = currentMillis()
+    courses = requests.get(
+            f'{protocol}://{hostname}/course/?locationId={locationId}&apiKey={apiKey}'
+                    ).json()
+
+    periods = requests.get(
+            f'{protocol}://{hostname}/period/?initialTimeBegin={millis}&count=20&apiKey={apiKey}'
+                    ).json()#.sort(key = lambda p: p['initialTime'])
+
     updateInfo()
 
 # Load the config file
 with open('innexgo-client.json') as configfile:
     config = json.load(configfile)
 
-    apiKey = config['apiKey']
     hostname = config['hostname']
+    protocol = config['protocol']
+    apiKey = config['apiKey']
     locationId = config['locationId']
 
     if apiKey is None or hostname is None or locationId is None:
         print('error reading the json')
         sys.exit()
 
-    courses = 
+    setInterval(updateInfoInfrequent, 1);
+
 
     # now we can get scanning
-    reader = mfrc522.MFRC522()
-
-    try:
-        while True:
-            (detectstatus, tagtype) = reader.MFRC522_Request(reader.PICC_REQIDL)
-            if detectstatus == reader.MI_OK:
-                (uidstatus, uid) = reader.MFRC522_Anticoll()
-                if uidstatus == reader.MI_OK:
-                    print(uid[0], uid[1], uid[2], uid[3])
-
-                time.sleep(0.01)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+#    reader = mfrc522.MFRC522()
+#
+#    try:
+#        while True:
+#            (detectstatus, tagtype) = reader.MFRC522_Request(reader.PICC_REQIDL)
+#            if detectstatus == reader.MI_OK:
+#                (uidstatus, uid) = reader.MFRC522_Anticoll()
+#                if uidstatus == reader.MI_OK:
+#                    print(uid[0], uid[1], uid[2], uid[3])
+#
+#                time.sleep(0.01)
+#    except KeyboardInterrupt:
+#        GPIO.cleanup()
