d5041543470668974dcc45294dba593898d985e8
gpi <gpimpale29@gmail.com>
2019-09-12 14:40:23 -0700

    edits

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
-sudo cp ./innexgo-client.json /mnt/boot
-sudo cp ./wpa_supplicant.conf /mnt/boot
+sudo touch /boot/ssh
+sudo cp ./innexgo-client.json /mnt/
+sudo cp ./wpa_supplicant.conf /mnt/
 
