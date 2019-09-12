db75b8b686906c700be71094babd9761feabc3fd
gpi <gpimpale29@gmail.com>
2019-09-11 18:47:36 -0700

    fixed bugs

--- client.py
+++ client.py
@@ -4,11 +4,12 @@ import os
 import sys
 import time
 import json
-# import mfrc522
+import mfrc522
+import datetime
 import requests
 import operator
 import threading
-#import RPi.GPIO
+import RPi.GPIO
 
 courses = None
 periods = None
@@ -33,19 +34,40 @@ def setInterval(func, sec):
     t.start()
     return t
 
+def printMillis(millis):
+    print(datetime.datetime.fromtimestamp(millis/1000.0))
 
 def updateInfo():
     global currentCourse
     global currentPeriod
 
     millis = currentMillis()
-    print(periods)
-    currentPeriod = filter(lambda p : (p['initialTime'] > millis), periods)[0]
-    print(currentPeriod)
 
-    currentCourse = filter(lambda c : (c['period'] == currentPeriod['period']), courses)[0]
-    print(currentCourse)
+    if periods is not None:
+        # get first period that is current
+        currentPeriod = next(
+            filter(
+                lambda p : (millis > p['initialTime'] and millis < p['endTime']),
+                periods
+            ),
+            None
+        )
+    else:
+        print('period is none')
+        currentCourse = None
 
+    if (courses is not None) and (currentPeriod is not None):
+        # get course with current location and period
+        currentCourse = next(
+            filter(
+                lambda c : (c['period'] == currentPeriod['period']),
+                courses
+            ),
+            None
+        )
+    else:
+        print('courses is none or currentPeriod is none')
+        currentCourse = None
 
 def updateInfoInfrequent():
     global periods
@@ -57,9 +79,9 @@ def updateInfoInfrequent():
                     ).json()
 
     periods = requests.get(
-            f'{protocol}://{hostname}/period/?initialTimeBegin={millis}&count=20&apiKey={apiKey}'
-                    ).json()#.sort(key = lambda p: p['initialTime'])
-
+            f'{protocol}://{hostname}/period/?apiKey={apiKey}'
+                    ).json()
+    periods = sorted(periods, key = lambda i: i['initialTime'])
     updateInfo()
 
 # Load the config file
@@ -79,16 +101,16 @@ with open('innexgo-client.json') as configfile:
 
 
     # now we can get scanning
-#    reader = mfrc522.MFRC522()
-#
-#    try:
-#        while True:
-#            (detectstatus, tagtype) = reader.MFRC522_Request(reader.PICC_REQIDL)
-#            if detectstatus == reader.MI_OK:
-#                (uidstatus, uid) = reader.MFRC522_Anticoll()
-#                if uidstatus == reader.MI_OK:
-#                    print(uid[0], uid[1], uid[2], uid[3])
-#
-#                time.sleep(0.01)
-#    except KeyboardInterrupt:
-#        GPIO.cleanup()
+    reader = mfrc522.MFRC522()
+
+    try:
+        while True:
+            (detectstatus, tagtype) = reader.MFRC522_Request(reader.PICC_REQIDL)
+            if detectstatus == reader.MI_OK:
+                (uidstatus, uid) = reader.MFRC522_Anticoll()
+                if uidstatus == reader.MI_OK:
+                    print(uid[0], uid[1], uid[2], uid[3])
+
+                time.sleep(0.01)
+    except KeyboardInterrupt:
+        Rpi.GPIO.cleanup()
